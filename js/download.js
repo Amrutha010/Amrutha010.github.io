fcf323b43a9586027522a585ec09f4f75144b515
Amrutha010 <karrinagadurgaamrutha040@gmail.com>
2023-03-31 12:15:07 +0530

    portfolio completed

--- js/download.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("dual_fn_res").addEventListener("click", dual_fn);
-function dual_fn() {
-    window.open(
-        'https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing',
-        '_blank' // <- This is what makes it open in a new window.
-      );
-      
-    // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
-}
